252b3edf8b78c8c59f9dca7abc719fefaae523cb
Valdeir\vnascimento <valdeircn11gmail.com>
2023-11-30 15:53:51 -0300

    Lendo informações customizadas do JWT no Resource Serve

--- ebi-api/src/main/java/br/com/ebi/novo/api/config/ResourceServerConfig.java
+++ ebi-api/src/main/java/br/com/ebi/novo/api/config/ResourceServerConfig.java
@@ -6,7 +6,16 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -16,12 +25,30 @@ public class ResourceServerConfig {
     @Bean
     public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-            .antMatchers("/oauth2/**").authenticated()
-            .and()
-            .csrf().disable()
-            .cors().and()
-            .oauth2ResourceServer().jwt();
+                .antMatchers("/oauth2/**").authenticated()
+                .and()
+                .csrf().disable()
+                .cors().and()
+                .oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthenticationConverter());
         return http.formLogin(Customizer.withDefaults()).build();
     }
 
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            List<String> authorities = jwt.getClaimAsStringList("authorities");
+            if (authorities == null) {
+                return Collections.emptyList();
+            }
+
+            JwtGrantedAuthoritiesConverter authoritiesConverter = new JwtGrantedAuthoritiesConverter();
+            Collection<GrantedAuthority> grantedAuthorities = authoritiesConverter.convert(jwt);
+            grantedAuthorities.addAll(authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .toList());
+            return grantedAuthorities;
+        });
+        return converter;
+    }
+    
 }
